9f0e32c1e4de33e12bedbc80aca3a749452829cc
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2020-12-29 08:37:35 +0100

    Initial commit

---
+++ ch01-basics/src/main/java/de/ostfale/sbsecurity/Ch01App.java
@@ -0,0 +1,12 @@
+package de.ostfale.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch01App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch01App.class, args);
+    }
+}
---
+++ ch01-basics/src/test/java/de/ostfale/sbsecurity/Ch01AppTest.java
@@ -0,0 +1,12 @@
+package de.ostfale.sbsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Ch01AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
