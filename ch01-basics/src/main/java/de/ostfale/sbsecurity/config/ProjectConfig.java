86398ba6bc86a20eb28611120ca75aefe5ea337b
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2020-12-31 09:11:31 +0100

    Use Builder instead of Beans
    + don't mix builder and beans

--- ch01-basics/src/main/java/de/ostfale/sbsecurity/config/ProjectConfig.java
+++ ch01-basics/src/main/java/de/ostfale/sbsecurity/config/ProjectConfig.java
@@ -1,17 +1,40 @@
 package de.ostfale.sbsecurity.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailsService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("max")
+                .password("test123")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
 
-    @Bean
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        // all requests needs to be authenticated
+        http.authorizeRequests()
+                .anyRequest()
+                //    .permitAll()  disable authentication
+                .authenticated();
+    }
+
+    // Declaration using Beans
+    /*   @Bean
     public UserDetailsService userDetailsService() {
         var userDetailsService = new InMemoryUserDetailsManager();
         var user = User.withUsername("max")
@@ -26,5 +49,7 @@ public class ProjectConfig {
     public PasswordEncoder passwordEncoder() {
         // used for test purpose only
         return NoOpPasswordEncoder.getInstance();
-    }
+    }*/
+
+
 }
