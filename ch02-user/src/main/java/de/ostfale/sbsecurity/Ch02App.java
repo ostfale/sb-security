f76f57e312316d75f3e8e69466e76d8569e53d1c
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-01-04 07:01:27 +0100

    Prepared chapter 2 - User
    + create new module
    + add README and configuration file
    + add main application

---
+++ ch02-user/src/main/java/de/ostfale/sbsecurity/Ch02App.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch02App.class, args);
+    }
+
+}
