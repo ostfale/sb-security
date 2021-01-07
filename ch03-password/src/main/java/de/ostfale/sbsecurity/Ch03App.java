fbaac1c39eed5a2d03ed743e224767e33f31a68b
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-01-07 08:07:42 +0100

    Prepared chapter 3 - passwords
    + add application class
    + add property file
    + create packages

---
+++ ch03-password/src/main/java/de/ostfale/sbsecurity/Ch03App.java
@@ -0,0 +1,12 @@
+package de.ostfale.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch03App {
+    public static void main(String[] args) {
+        SpringApplication.run(Ch03App.class, args);
+    }
+
+}
