c9b0be63bcc1e0ef412c5a9f4dac3d9fc5a39b54
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-01-12 08:06:18 +0100

    Prepared Chapter 5
    + renamed chapter 4

--- ch04-password/src/main/java/de/ostfale/sbsecurity/Ch04App.java
+++ ch04-password/src/main/java/de/ostfale/sbsecurity/Ch04App.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ch03App {
+public class Ch04App {
     public static void main(String[] args) {
-        SpringApplication.run(Ch03App.class, args);
+        SpringApplication.run(Ch04App.class, args);
     }
 
 }
---
+++ ch05-authentification/src/main/java/de/ostfale/sbsecurity/Ch05App.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch05App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch05App.class, args);
+    }
+
+}
