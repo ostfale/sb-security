f6a05def1e358dc611028dfaff91dcffbc204f3a
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-01-14 08:19:26 +0100

    DelegatingSecurityContextCallable
    + propagate Security Context to an external thread

--- ch05-authentication/README.md
+++ ch05-authentication/README.md
@@ -3,4 +3,18 @@
 ````shell
 curl -u max:test1234 http://localhost:8080/hello                    # explicit security context
 curl -u max:test1234 http://localhost:8080/helloInject              # injected security context
-``
+````
+
+### Security Context Models
+
++ MODE_THREADLOCAL (default)
++ MODE_INHERITEDTHREADLOCAL (async)
++ MODE_GLOBAL (standalone application)
+
+### DelegatingSecurityContextCallable
+
+Provides Security Context to external threads
+
+````shell
+curl -u max:test1234 http://localhost:8080/ciao                    # delegated security context
+````
--- ch05-authentication/src/main/java/de/ostfale/sbsecurity/controller/HelloController.java
+++ ch05-authentication/src/main/java/de/ostfale/sbsecurity/controller/HelloController.java
@@ -1,11 +1,16 @@
 package de.ostfale.sbsecurity.controller;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -28,5 +33,21 @@ public class HelloController {
         SecurityContext context = SecurityContextHolder.getContext();
         String username = context.getAuthentication().getName();
         System.out.println("Username from async context: " + username);
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            return securityContext.getAuthentication().getName();
+        };
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        try {
+            // needs DelegatingSecurityContextCallable to provide Context otherwise NPE
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + executorService.submit(contextTask).get() + "!";
+        } finally {
+            executorService.shutdown();
+        }
     }
 }
