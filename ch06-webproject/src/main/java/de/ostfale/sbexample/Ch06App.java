a5abae4cb1e98a1e2a6cf9bc42986ec4e6447167
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-01-15 08:39:51 +0100

    Setup for chapter 6 with an example project

---
+++ ch06-webproject/src/main/java/de/ostfale/sbexample/Ch06App.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch06App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch06App.class, args);
+    }
+
+}
