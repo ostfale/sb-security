4900e0990bd880d699fac3e4054c4fb6ea7d7765
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-01-25 06:51:44 +0100

    Init chapter 08
    + configuration of authorization

---
+++ ch08-cfgauth/src/main/java/de/ostfale/sbexample/Ch08App.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch08App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch08App.class, args);
+    }
+
+}
