cf8399e8481cf74dc7e820198872b85552767366
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-01-29 08:05:38 +0100

    CSRF for LoginForm
    + use a login form for POST with CSRF token

--- ch10-cors/src/main/java/de/ostfale/sbsecurity/config/ProjectConfig.java
+++ ch10-cors/src/main/java/de/ostfale/sbsecurity/config/ProjectConfig.java
@@ -1,17 +1,40 @@
 package de.ostfale.sbsecurity.config;
 
 import de.ostfale.sbsecurity.filter.CsrfTokenLogger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public UserDetailsService uds() {
+        var uds = new InMemoryUserDetailsManager();
+        var mary = User.withUsername("mary")
+                .password("12345")
+                .authorities("READ")
+                .build();
+        uds.createUser(mary);
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
-                .authorizeRequests().anyRequest().permitAll();
+                .authorizeRequests().anyRequest().authenticated();
+        http.formLogin().defaultSuccessUrl("/main", true);
     }
 }
---
+++ ch10-cors/src/main/java/de/ostfale/sbsecurity/controller/MainController.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main() {
+        return "main.html";
+    }
+}
