7822e8c90224833970826c44e662d7cf52ef3159
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-02-04 08:32:13 +0100

    Prepared chapter 11 business logic server
    + dependencies for jwt
    + package structure

---
+++ ch11-blserver/src/main/java/de/ostfale/sbsecurity/Ch11BusinessLogicServerApp.java
@@ -0,0 +1,11 @@
+package de.ostfale.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch11BusinessLogicServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Ch11BusinessLogicServerApp.class, args);
+    }
+}
